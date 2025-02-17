04a66d4f00be2741e4bc08e24c87467daf9245d6
고용수 <ajemxkem@gmail.com>
2025-02-18 01:09:32 +0900

    Reducer

---
+++ src/reducer/person-reducer.js
@@ -0,0 +1,31 @@
+export default function personReducer(person, action) {
+    switch(action.type) {
+        case 'updated': {
+            const { prev, current } = action;
+            return {
+                ...person,
+                mentors: person.mentors.map((mentor) => {
+                    return mentor.name === prev ? {...mentor, name: current } : mentor;
+                })
+            };
+        }
+        case 'added' : {
+            const { name, title } = action;
+            return {
+                ...person,
+                mentors: [...person.mentors, { name, title }],
+            };
+        }
+        case 'deleted' : {
+            return {
+                ...person,
+                mentors: person.mentors.filter(
+                    (mentor) => mentor.name !== action.name
+                ),
+            };
+        }
+        default: {
+            throw Error(`알수없는 액션 타입이다: ${action.type}`);
+        }
+    }
+}
